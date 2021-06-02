3ae21f4a6cf0172f165755d86c6cbf3ff731235a
Spencer Krause <spencerk@skyward.com>
2021-06-02 13:39:09 -0500

    request

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        private const string aPIKey = "key=1d28751350144a4e835b8e6a355f9113";
+        private const string aPIKey = "?key=1d28751350144a4e835b8e6a355f9113";
         private const string uRL = "https://api.rawg.io/api/";
         static void Main(string[] args)
         {
-
-        }
-
-        private static void APIRequest()
-        {
-            private List<string> games = new List<string>();
+            var client = new WebClient();
+            //string response = client.DownloadString($"{uRL}games{aPIKey}");
+            //System.IO.File.WriteAllText("games.txt", response);
+            string response = System.IO.File.ReadAllText("games.txt");
         }
     }
 }
