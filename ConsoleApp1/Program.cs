d0f328278cdd618014e6c93368a3785b6eb4303c
Spencer K <thelostdreamer1@gmail.com>
2021-06-02 12:57:12 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private const string aPIKey = "key=1d28751350144a4e835b8e6a355f9113";
+        private const string uRL = "https://api.rawg.io/api/";
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void APIRequest()
+        {
+            private List<string> games = new List<string>();
+        }
+    }
+}
